d4ce5758b11ac97b2e95b01bc7f97122784f1da7
Shinichi Yamashita <s.yamashita@jcom.zaq.ne.jp>
2024-07-03 22:28:54 +0900

    chg: gui parts

--- labs/refimg/sfdr.py
+++ labs/refimg/sfdr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,20 +11,29 @@ import tkinter as tk
 def dB(x):
   return 10*np.log10(np.abs(x))
 
-def spectra(fc, fs, N, Nb, K2, K3):
+def INL(s, K2, K3, ls):
+   return -s**3 * 10 ** -K3 - s**2 * 10**-K2 + s
+   
+def INLm(s, K2, K3, ls, slice, ms):
+   m = ((s + 1.0) % 0.25) * ls / 512
+   if slice == int((s + 1.0) / 0.25):
+      m += ms / 512 *0.25
+   return -s**3 * 10 ** -K3 - s**2 * 10**-K2 + s + m
+   
+def spectra(fc, fs, N, Nb, K2, K3, ls, amp, slice, ms):
   y = []
   for i in range(N):
     t = i / fs
-    s = math.sin(math.pi * 2 * fc * t)
-    s = -s**3 * 10 ** -K3 - s**2 * 10**-K2 + s
+    s = math.sin(math.pi * 2 * fc * t) * 10 ** (amp/20)
+    s = INLm(s, K2, K3, ls, slice, ms)
     for b in range(Nb):
         y.append(s)
 
-  fr, Gxx = sig.welch(y, fs=fs*Nb, window='blackman', nperseg=N, scaling='spectrum')
+  fr, Gxx = sig.welch(y, fs=fs*Nb, window='flattop', nperseg=N, scaling='spectrum')
   inl = []
   for i in range(4096):
     s = i / 2048 - 1.0
-    sd = -s**3 * 10 ** -K3 - s**2 * 10**-K2 + s
+    sd = INLm(s, K2, K3, ls, slice, ms)
     inl.append(sd-s)
   inl = np.array(inl) - ((inl[-1]-inl[0]) * (np.arange(4096)/4096) + inl[0])
   return fr, Gxx, inl
@@ -40,7 +49,7 @@ class Application(tk.Tk):
     def create_widgets(self):
         '''ウィジェットの配置'''
         self.canvas_frame = tk.Frame(self)
-        self.canvas_frame.pack(side=tk.LEFT)
+        self.canvas_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.control_frame = tk.Frame(self)
         self.control_frame.pack(side=tk.RIGHT)
 
@@ -49,36 +58,45 @@ class Application(tk.Tk):
         #self.fig = plt.figure() #figsize=(5, 5))
         self.canvas = FigureCanvasTkAgg(self.fig, self.canvas_frame)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
+        self.canvas.get_tk_widget().pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.canvas_frame)
+        self.toolbar.pack()
         # スライダーの配置
-        self.control_label = tk.Label(self.control_frame, text='fc:')
-        self.control_label.pack(anchor=tk.NW)
-        self.fc = tk.DoubleVar()
-        self.x_scale = tk.Scale(self.control_frame,
-            variable=self.fc, from_=1.0, to=16.0, resolution=0.1, orient=tk.HORIZONTAL)
-        self.fc.set(8)
-        self.x_scale.pack(anchor=tk.NW)
-        label = tk.Label(self.control_frame, text='k3:')
-        label.pack(anchor=tk.NW)
-        self.k3 = tk.DoubleVar()
-        k3= tk.Scale(self.control_frame,
-            variable=self.k3, from_=1.0, to=8.0, resolution=0.5, orient=tk.HORIZONTAL)
-        self.k3.set(2)
-        k3.pack(anchor=tk.NW)
-        label = tk.Label(self.control_frame, text='k2:')
-        label.pack(anchor=tk.NW)
-        self.k2 = tk.DoubleVar()
-        k2= tk.Scale(self.control_frame,
-            variable=self.k2, from_=1.0, to=8.0, resolution=0.5, orient=tk.HORIZONTAL)
-        self.k2.set(2)
-        k2.pack(anchor=tk.NW)
+        self.fc = self.slider("fc", 0.9, 1.0, 16.0, 0.05)
+        self.k3 = self.slider("k3", 3.5, 1.0, 8.0, 0.5)
+        self.k2 = self.slider("k2", 3.5, 1.0, 8.0, 0.5)
+        self.ls = self.slider("ls", 0, -5.0, 5.0, 0.5)
+        self.amp = self.slider("amp", 0, 0.0, -30.0, 0.5)
+        self.slice = self.slider("slice", 0, 0, 7, 1)
+        self.ms = self.slider("ms", 0, -5, 5, 0.5)
 
-    def update_anim(self, dt):
+
+    def slider(self, name='k', init=2, from_=1.0, to=8.0, resolution=0.5):
+        frm = tk.Frame(self)
+        label = tk.Label(frm, text=name)
+        label.pack(side=tk.LEFT)
+        k = tk.DoubleVar()
+        ks= tk.Scale(frm, variable=k, from_=from_, to=to, resolution=resolution, orient=tk.HORIZONTAL, command=self.draw_plot)
+        k.set(init)
+        ks.pack(side=tk.RIGHT)
+        frm.pack()
+        return k
+
+    def draw_plot(self, *args):
         '''グラフ更新関数'''
-        fr, gxx, inl = spectra(self.fc.get(), 32, 8192, 8, self.k2.get(), self.k3.get())
+        fc = self.fc.get()
+        fr, gxx, inl = spectra(fc, 32, 8192, 8, 
+                    self.k2.get(), self.k3.get(), self.ls.get(), self.amp.get(), self.slice.get(), self.ms.get())
+        fr = fr[:512]
+        gxx = gxx[:512]
+        #ifc = fc/32*8192/8
+        #print(fc, ifc)
+        peaks, properties = sig.find_peaks(dB(gxx), prominence=10, height=-90)
+        print(peaks, dB(gxx[peaks]))
+
         self.ax[0].clear()
         self.ax[0].plot(fr, dB(gxx))
+        self.ax[0].plot(fr[peaks], dB(gxx[peaks]), "x")
         self.ax[0].set_ylim(-100,0)
         self.ax[0].set_xlim(0,16)
         self.ax[0].set_ylabel("spectrum(dB)")
@@ -92,17 +110,8 @@ class Application(tk.Tk):
         self.ax[1].set_xlabel("code")
         self.ax[1].grid()
         self.fig.tight_layout()
-
-    def draw_plot(self, event=None):
-
-        self.ani = FuncAnimation(
-                  self.fig,  # Figureオブジェクト
-                  self.update_anim,  # グラフ更新関数
-                  init_func=None,  # 初期化関数
-                  interval = 100,  # 更新間隔(ms)
-                  blit = False,
-                  )
         self.canvas.draw()
+
 
 
 app = Application()
